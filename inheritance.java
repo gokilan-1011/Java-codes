a416c06641276dbe011db62dd0c8200bde55bf3f
gokilan-1011 <kalagokilan@gmail.com>
2026-07-23 15:26:36 +0530

    Create inheritance.java

---
+++ inheritance.java
@@ -0,0 +1,20 @@
+class parent{
+    void display(){
+        System.out.println("This is the parent class");
+    }
+}
+class child extends parent
+{
+    void show(){
+        System.out.println("This is the child class");
+    }
+}
+public class inheritance{
+    public static void main(String[] args){
+        child obj=new child();
+        obj.display();
+        obj.show();
+        
+    }
+}
+
